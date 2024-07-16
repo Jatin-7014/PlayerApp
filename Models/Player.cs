7b023a63403729f332109d1e6d06bf7131062132
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-16 12:31:04 +0530

    Initial Commit

---
+++ Models/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Models
+{
+    internal class Player
+    {
+        static int MIN_AGE = 18;
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; } = MIN_AGE;
+
+        public Player(int id,string name) 
+        {
+            Id = id;
+            Name = name;
+        }
+        public Player(int id, string name,int age):this(id,name)
+        {
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}\nAge:{Age}\nId:{Id}\n";
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            int maxAge = MIN_AGE;
+            Player eldestPlayer = null;
+            foreach (Player player in players)
+            {
+                if (player.Age > maxAge)
+                {
+                    maxAge = player.Age;
+                    eldestPlayer = player;
+                }
+            }
+            return eldestPlayer;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PlayerApp.Models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[] {new Player(101,"Jatin",22),
+                new Player(102,"Anil"),
+                new Player(103,"Saroj",23),
+                new Player(104,"XyZ"),
+                new Player(105,"ABC",30)};
+
+            Player Eldest=Player.WhoIsElder(players);
+            Console.WriteLine("===========Eldest Player===========");
+            Console.WriteLine(Eldest);
+        }
+    }
+}
